2ed190f306ddbc235d8d75f0e4f7ecaedad199d6
Markus Scheuring <markus-scheuring@gmx.de>
2020-05-11 19:01:26 +0200

    Create Project
    Create Project

---
+++ app/src/main/java/dhbw/lichter/scheuring/formelapp/ui/database/DatabaseFragment.java
@@ -0,0 +1,20 @@
+package dhbw.lichter.scheuring.formelapp.ui.database;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import dhbw.lichter.scheuring.formelapp.R;
+
+public class DatabaseFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_database, container, false);
+        return root;
+    }
+}
