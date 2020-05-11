cc52393b24614f458db1532d3652bd1a885dd2b5
Markus Scheuring <markus-scheuring@gmx.de>
2020-05-11 20:15:31 +0200

    Add Button and EventListener
    Add Button in View
    Add Values from Inputs in Home Fragment
    Add EventListener in Home Fragment

--- app/src/main/java/dhbw/lichter/scheuring/formelapp/ui/home/HomeFragment.java
+++ app/src/main/java/dhbw/lichter/scheuring/formelapp/ui/home/HomeFragment.java
@@ -4,21 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import dhbw.lichter.scheuring.formelapp.R;
 
 public class HomeFragment extends Fragment {
 
+    private EditText editTextIntensity;
+    private EditText editTextLength;
+    private EditText editTextNumberKids;
+    private EditText editTextAgeListeners;
+    private Spinner spnSocailEmbarressment;
+    private Spinner spnGenderFactor;
+    private Button btnCreateFart;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //Mit View Elementen Verknüpfen
+        editTextIntensity = (EditText) root.findViewById(R.id.editText_fartIntensity);
+        editTextLength = (EditText) root.findViewById(R.id.editText_fartLength);
+        editTextNumberKids = (EditText) root.findViewById(R.id.editText_number_kids_present);
+        editTextAgeListeners = (EditText) root.findViewById(R.id.editText_age_of_listener);
+        btnCreateFart = (Button) root.findViewById(R.id.btn_create_fart);
+        //EventListener für Button hinzufügen
+        btnCreateFart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateTitle();
+            }
+        });
         return root;
+    }
+
+    public void updateTitle() {
+        editTextIntensity.setText("Test");
     }
 }
