811333d37dbd6a5e553d798e9f095ab4b0e19db6
Markus Scheuring <markus-scheuring@gmx.de>
2020-05-22 18:13:08 +0200

    Add Details in formula
    Add TextViews for every factor and set Data

--- app/src/main/java/dhbw/lichter/scheuring/formelapp/ui/detail/DetailFragment.java
+++ app/src/main/java/dhbw/lichter/scheuring/formelapp/ui/detail/DetailFragment.java
@@ -15,6 +15,13 @@ import io.github.kexanie.library.MathView;
 public class DetailFragment extends Fragment {
 
     public MathView formula;
+    public TextView intensity;
+    public TextView length;
+    public TextView embarrassment;
+    public TextView numberKids;
+    public TextView ageListener;
+    public TextView genderFactor;
+    public TextView result;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,6 +29,13 @@ public class DetailFragment extends Fragment {
 
         //Mit View Element verknüpfen
         formula = (MathView) root.findViewById(R.id.detail_formula);
+        intensity = (TextView) root.findViewById(R.id.txtView_detail_intensity);
+        length = (TextView) root.findViewById(R.id.txtView_detail_length);
+        embarrassment = (TextView) root.findViewById(R.id.txtView_detail_embarrassment);
+        numberKids = (TextView) root.findViewById(R.id.txtView_detail_number_kids);
+        ageListener = (TextView) root.findViewById(R.id.txtView_detail_age_listeners);
+        genderFactor = (TextView) root.findViewById(R.id.txtView_detail_gender_factor);
+        result = (TextView) root.findViewById(R.id.txtView_detail_result);
 
         //getData
         Bundle bundle = getArguments();
@@ -30,15 +44,12 @@ public class DetailFragment extends Fragment {
         int valueEmbarrassment = bundle.getInt("embarrassment");
         int valueNumberKids = bundle.getInt("numberKids");
         int valueAgeListeners = bundle.getInt("ageListeners");
-        int valueGenderFactor = bundle.getInt("genderFactor");
-        double result = bundle.getDouble("result");
-        String genderFactor = bundle.getString("strGenderFactor");
-        String socialEmbarrassment = bundle.getString("strSocialEmbarrassment");
+        double valueGenderFactor = bundle.getDouble("genderFactor");
+        double valueResult = bundle.getDouble("result");
+        String strGenderFactor = bundle.getString("strGenderFactor");
 
         //setData
-        String strFormula = "$$"
-                .concat(String.valueOf(result))
-                .concat(" = \\frac{(")
+        String strFormula = "$$\\frac{("
                 .concat(String.valueOf(valueIntensity))
                 .concat(" * ")
                 .concat(String.valueOf(valueLength))
@@ -50,8 +61,17 @@ public class DetailFragment extends Fragment {
                 .concat(String.valueOf(valueAgeListeners))
                 .concat(" * ")
                 .concat(String.valueOf(valueGenderFactor))
-                .concat(")}$$");
+                .concat(")} = ")
+                .concat(String.valueOf((int) valueResult))
+                .concat("$$");
         formula.setText(strFormula);
+        intensity.setText(getResources().getString(R.string.detail_intensity).concat(String.valueOf(" " + valueIntensity + " db")));
+        length.setText(getResources().getString(R.string.detail_length).concat(String.valueOf(" " + valueLength + " Sekunden")));
+        embarrassment.setText(getResources().getString(R.string.detail_social_embarrassment).concat(String.valueOf(" " + valueEmbarrassment)));
+        numberKids.setText(getResources().getString(R.string.detail_number_kids).concat(String.valueOf(" " + valueNumberKids)));
+        ageListener.setText(getResources().getString(R.string.detail_age_listener).concat(String.valueOf(" " + valueAgeListeners)));
+        genderFactor.setText(getResources().getString(R.string.detail_gender_factor).concat(strGenderFactor + " mit Wert " + valueGenderFactor));
+        result.setText(getResources().getString(R.string.detail_result).concat(String.valueOf(" " + valueResult)));
 
         return root;
     }
