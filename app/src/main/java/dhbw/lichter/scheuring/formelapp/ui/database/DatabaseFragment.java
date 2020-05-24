b25a8093d0afafaa1eca0e649b2500349066816a
TischBacchus021 <lichteransgar@gmail.com>
2020-05-24 14:23:43 +0200

    database fragment: get object of database manager

--- app/src/main/java/dhbw/lichter/scheuring/formelapp/ui/database/DatabaseFragment.java
+++ app/src/main/java/dhbw/lichter/scheuring/formelapp/ui/database/DatabaseFragment.java
@@ -1,5 +1,6 @@
 package dhbw.lichter.scheuring.formelapp.ui.database;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +10,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import dhbw.lichter.scheuring.formelapp.R;
+import dhbw.lichter.scheuring.formelapp.util.DatabaseManager;
 
 public class DatabaseFragment extends Fragment {
+    protected Activity _activity;
+    protected DatabaseManager dbHelper;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_database, container, false);
+
+        dbHelper = new DatabaseManager(getActivity());
         return root;
     }
 }
