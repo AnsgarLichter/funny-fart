689e9120cf67208eefce7711caad2070e7133004
Ansgar Lichter <LichterAnsgar@gmail.com>
2020-05-26 11:44:45 +0200

    show formula with MathView

--- app/src/main/java/dhbw/lichter/scheuring/formelapp/ui/help/HelpFragment.java
+++ app/src/main/java/dhbw/lichter/scheuring/formelapp/ui/help/HelpFragment.java
@@ -9,12 +9,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import dhbw.lichter.scheuring.formelapp.R;
+import io.github.kexanie.library.MathView;
 
 public class HelpFragment extends Fragment {
+    public MathView formula;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_help, container, false);
+
+        formula = (MathView) root.findViewById(R.id.help_formula);
+        String strFormula = "$$\\frac{(I * L)^S * K}{(A * g)} = F$$";
+        formula.setText(strFormula);
         return root;
     }
 }
