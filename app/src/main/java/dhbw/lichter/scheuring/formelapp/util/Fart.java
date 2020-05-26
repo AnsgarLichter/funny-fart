4c002ac790f9a5195d81f0bf67c6592c8a92d29a
Ansgar Lichter <LichterAnsgar@gmail.com>
2020-05-26 11:06:05 +0200

    implemented method "prepareInsertStatement"

--- app/src/main/java/dhbw/lichter/scheuring/formelapp/util/Fart.java
+++ app/src/main/java/dhbw/lichter/scheuring/formelapp/util/Fart.java
@@ -1,5 +1,7 @@
 package dhbw.lichter.scheuring.formelapp.util;
 
+import android.database.sqlite.SQLiteStatement;
+
 public class Fart {
     private int intensity;
     private int length;
@@ -8,61 +10,74 @@ public class Fart {
     private int averageAge;
     private double score;
     private String sex;
+    private String name;
+    private String insert_fart;
 
-    public Fart (int intensity, int length, int socialEmbarrassment, int countChildren, int averageAge, String sex) {
-       this.intensity = intensity;
-       this.length = length;
-       this.socialEmbarrassment = socialEmbarrassment;
-       this.countChildren = countChildren;
-       this.averageAge = averageAge;
-       this.sex = sex;
+    public Fart(int intensity, int length, int socialEmbarrassment, int countChildren, int averageAge, String sex) {
+        this.intensity = intensity;
+        this.length = length;
+        this.socialEmbarrassment = socialEmbarrassment;
+        this.countChildren = countChildren;
+        this.averageAge = averageAge;
+        this.sex = sex;
     }
 
-    public int getIntensity () {
+    public SQLiteStatement prepareInsertStatement(SQLiteStatement insertFart) {
+        insertFart.bindDouble(1, score);
+        insertFart.bindString(2, name);
+        insertFart.bindLong(3, intensity);
+        insertFart.bindLong(4, length);
+        insertFart.bindLong(5, socialEmbarrassment);
+        insertFart.bindLong(6, countChildren);
+        insertFart.bindDouble(7, averageAge);
+        insertFart.bindString(8, sex);
+
+        return insertFart;
+    }
+
+    public int getIntensity() {
         return intensity;
     }
 
-    public void setIntensity (int intensity) {
+    public void setIntensity(int intensity) {
         this.intensity = intensity;
     }
 
-    public int getLength () {
+    public int getLength() {
         return length;
     }
 
-    public void setLength (int length) {
+    public void setLength(int length) {
         this.length = length;
     }
 
-    public int getSocialEmbarrassment () {
+    public int getSocialEmbarrassment() {
         return socialEmbarrassment;
     }
 
-    public void setSocialEmbarrassment (int socialEmbarrassment) {
-        this.socialEmbarrassment = socialEmbarrassment;
-    }
+    public void setSocialEmbarrassment(int socialEmbarrassment) { this.socialEmbarrassment = socialEmbarrassment; }
 
-    public int getCountChildren () {
+    public int getCountChildren() {
         return countChildren;
     }
 
-    public void setCountChildren (int countChildren) {
+    public void setCountChildren(int countChildren) {
         this.countChildren = countChildren;
     }
 
-    public int getAverageAge () {
+    public int getAverageAge() {
         return averageAge;
     }
 
-    public void setAverageAge (int averageAge) {
+    public void setAverageAge(int averageAge) {
         this.averageAge = averageAge;
     }
 
-    public String getSex () {
+    public String getSex() {
         return sex;
     }
 
-    public void setSex (String sex) {
+    public void setSex(String sex) {
         this.sex = sex;
     }
 
@@ -70,11 +85,11 @@ public class Fart {
         return score;
     }
 
-    public void setScore (double score) {
+    public void setScore(double score) {
         this.score = score;
     }
 
-    public String ToInsertString () {
-        return "" + score + ", " + intensity + ", " + length + ", " + socialEmbarrassment + ", " + countChildren + ", " + averageAge + ", " + sex;
-    }
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
 }
