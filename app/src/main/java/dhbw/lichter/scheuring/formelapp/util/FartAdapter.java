4c633b6058f1c233cecb1bc1c51bc295b5c296be
Ansgar Lichter <LichterAnsgar@gmail.com>
2020-05-27 12:23:16 +0200

    closes #2, closes #3: implemented logic of delete and detail buttons

--- app/src/main/java/dhbw/lichter/scheuring/formelapp/util/FartAdapter.java
+++ app/src/main/java/dhbw/lichter/scheuring/formelapp/util/FartAdapter.java
@@ -10,14 +10,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 import dhbw.lichter.scheuring.formelapp.R;
+import dhbw.lichter.scheuring.formelapp.ui.database.DatabaseFragment;
 import lombok.NonNull;
 
 public class FartAdapter extends RecyclerView.Adapter<FartViewHolder> {
 
+    private final DatabaseFragment dbFragment;
     private List<Fart> farts;
+    private DatabaseManager dbHelper;
 
-    public FartAdapter(List<Fart> farts){
+    public FartAdapter(List<Fart> farts, DatabaseManager dbHelper, DatabaseFragment dbFragment) {
         this.farts = farts;
+        this.dbHelper = dbHelper;
+        this.dbFragment = dbFragment;
     }
 
     @Override
@@ -28,7 +33,7 @@ public class FartAdapter extends RecyclerView.Adapter<FartViewHolder> {
     @Override
     public FartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_card_template, parent, false);
-        return new FartViewHolder(view);
+        return new FartViewHolder(view, dbFragment, this);
     }
 
     @Override
@@ -38,5 +43,10 @@ public class FartAdapter extends RecyclerView.Adapter<FartViewHolder> {
         fartViewHolder.fartScore.setText("" + (int) farts.get(i).getScore());
         fartViewHolder.creationDate.setText(farts.get(i).getCreationDate());
         fartViewHolder.fartGif.setImageResource(R.drawable.im_funny_fart);
+    }
+
+    public void removeFart(int position) {
+        farts.remove(position);
+        notifyItemRemoved(position);
     }
 }
