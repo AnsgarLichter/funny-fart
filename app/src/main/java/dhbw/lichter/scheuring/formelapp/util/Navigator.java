323bd77d974ece0f1d5f2e6d2099f75c0f09f582
Ansgar Lichter <LichterAnsgar@gmail.com>
2020-06-09 21:44:57 +0200

    #17: Fix warnings Navigator

--- app/src/main/java/dhbw/lichter/scheuring/formelapp/util/Navigator.java
+++ app/src/main/java/dhbw/lichter/scheuring/formelapp/util/Navigator.java
@@ -7,7 +7,6 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import dhbw.lichter.scheuring.formelapp.R;
-import dhbw.lichter.scheuring.formelapp.ui.detail.DetailFragment;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,13 +19,6 @@ public class Navigator {
 
     public void navigate(Fragment fragment, Boolean addToBackStack, Bundle bundle) {
         fragment.setArguments(bundle);
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.nav_host_fragment, fragment);
-        if(addToBackStack) fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
-    }
-
-    public void navigate(Fragment fragment, Boolean addToBackStack) {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.nav_host_fragment, fragment);
         if(addToBackStack) fragmentTransaction.addToBackStack(null);
