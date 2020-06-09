d740a1ed16648ce6f6bcabffc4569a0ef8a8f541
Ansgar Lichter <LichterAnsgar@gmail.com>
2020-06-09 21:46:19 +0200

    #17: Fix warnings toaster

--- app/src/main/java/dhbw/lichter/scheuring/formelapp/util/Toaster.java
+++ app/src/main/java/dhbw/lichter/scheuring/formelapp/util/Toaster.java
@@ -12,22 +12,18 @@ import dhbw.lichter.scheuring.formelapp.R;
 public class Toaster {
 
     private Toast toast;
-//    private Context applicationContext;
     private View view;
     private ImageView icon;
     private TextView text;
 
     public Toaster(Context applicationContext, View view) {
-//        this.applicationContext = applicationContext;
         toast = new Toast(applicationContext);
         this.view = view;
         icon = view.findViewById(R.id.custom_toast_icon);
         text = view.findViewById(R.id.custom_toast_txt);
     }
 
-    //TODO: Apply different styles for error and success toasts
     public void showError(int resId) {
-//        Toast toast = Toast.makeText(applicationContext, applicationContext.getResources().getString(resId), Toast.LENGTH_LONG);
         icon.setImageResource(R.drawable.ic_toast_error);
         text.setText(resId);
         toast.setGravity(Gravity.BOTTOM, 0, 250);
@@ -37,7 +33,6 @@ public class Toaster {
     }
 
     public void showSuccess(int resId) {
-//        Toast toast = Toast.makeText(applicationContext, applicationContext.getString(resId), Toast.LENGTH_SHORT);
         icon.setImageResource(R.drawable.ic_toast_success);
         text.setText(resId);
         toast.setGravity(Gravity.BOTTOM, 0, 250);
